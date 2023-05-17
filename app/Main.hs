3c0749f761b8f0010afc0ec446774bf3596b6d37
Bruce C. Miller <bm3719@gmail.com>
2023-05-16 21:57:32 -0400

    Get URI parsing working

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
+
+import Data.Maybe (fromMaybe)
+import Network.URI (parseURI, uriScheme, uriAuthority, uriRegName)
 import System.Environment
+
+-- testUrl :: String
+-- testUrl = "https://www.example.com:8080/path/to/page?query=value#fragment"
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [] -> putStrLn "No URL specified"
-    url:_ -> putStrLn ("Navigating to: " ++ url)
+    url:_ -> do
+      putStrLn $ "Navigating to: " ++ url
+      let parsedUrl = parseURI url
+      putStrLn $ "URI Scheme: " ++ (fromMaybe "Invalid URL" $ uriScheme <$> parsedUrl)
+      putStrLn $ "Host: " ++ maybe "Invalid URL" uriRegName (uriAuthority =<< parseURI url)
