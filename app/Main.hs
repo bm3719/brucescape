0d9a2e21bb1c394cabd691adb05c2956704e434d
Bruce C. Miller <bm3719@gmail.com>
2023-08-03 23:59:43 -0400

    Get basic TUI working

--- app/Main.hs
+++ app/Main.hs
@@ -10,11 +10,66 @@ module Main where
 -- import           Data.List                      as DL (intercalate, stripPrefix)
 -- import           Data.Maybe                     (fromMaybe)
 -- import           GHC.Int
--- import qualified GHC.Word                       as W
+import Brick
 import Control.Lens
-import Data.Aeson.Lens (_String, key)
-import Network.Wreq
+import Network.HTTP.Client (HttpException(..))
+import Network.Wreq        as W
 import System.Environment
+-- import Data.Monoid ((<>))
+import Text.Wrap           (defaultWrapSettings, preserveIndentation)
+
+ui :: String -> Widget ()
+ui s =
+  strWrapWith (defaultWrapSettings { preserveIndentation = True }) $ s
+
+-- | Main entry point and future location of event handler.
+main :: IO ()
+main = do
+  getArgs >>= \case
+    [] -> putStrLn "No URL specified"
+    url:_ -> do
+      putStrLn $ "Navigating to: " ++ url
+      r <- W.get url
+      if r ^. responseStatus ^. statusCode == 200 then
+        simpleMain (ui $ show $ r ^. responseBody)
+      else putStrLn ("Error loading " ++ url)
+  where
+    handler :: HttpException -> IO ()
+    handler ex = putStrLn $ "Caught Exception: " ++ show ex
+
+-- testUrl1 = "http://www.example.com:8080/path/to/page?query=value#fragment"
+-- testUrl2 = "http://www.example.com"                           -- small HTTP page
+-- testUrl3 = "http://eap.mcgill.ca/MagRack/COG/COG_P_96_05.htm" -- large HTTP page
+-- testUrl4 = "http://eap.mcgill.ca/MagRack/COG/cognitio.htm"    -- large HTTP page
+-- testUrl5 = "http://reduction.io/essays/rosetta-haskell.html"  -- different chunk size
+
+
+      -- let parsedUrl = parseURI url
+      --     scheme = (fromMaybe "Invalid URL" $ (dropLastColon . uriScheme) <$> parsedUrl)
+      --     host = maybe "Invalid URL" uriRegName (uriAuthority =<< parseURI url)
+      --     path = (fromMaybe "Invalid URL" $
+      --             (('/' :) . DL.intercalate "/" . pathSegments) <$> parsedUrl)
+      -- -- putStrLn $ "URI Scheme: " ++ scheme
+      -- -- putStrLn $ "Host: " ++ host
+      -- -- putStrLn $ "Path: " ++ path
+
+      -- -- Create network connection to host.
+      -- (sock, addr) <- createSocket scheme host
+      -- connect sock (addrAddress addr)
+      -- let request = "GET " ++ path ++ " HTTP/1.1\r\nHost: " ++ host ++ "\r\n\r\n"
+      -- sendAll sock (BS.pack request)
+      -- response <- recvAllChunks sock 4096
+      -- close sock
+
+      -- -- Handle response content.
+      -- let servResp = word8ArrayToString $ BL.unpack $ mconcat response
+      -- let linesList = lines servResp
+      -- let [httpVer, statusCode, statusMsg, date, server, lastMod, acceptRanges] = take 7 linesList
+      -- let [httpVersion, status, explanation] = take 3 $ words httpVer
+      -- let content = unlines $ drop 8 linesList
+      -- mapM_ putStrLn [httpVersion, status, explanation, statusCode, statusMsg, date, server, lastMod, acceptRanges]
+      -- -- putStrLn content
+
 
 -- createSocket :: String -> String -> IO (Socket, AddrInfo)
 -- createSocket scheme host = do
@@ -44,47 +99,3 @@ import System.Environment
 --     else do
 --       chunks <- recvAllChunks sock bufferSize
 --       return (chunk : chunks)
-
--- | Main entry point and future location of event handler.
-main :: IO ()
-main = do
-  getArgs >>= \case
-    [] -> putStrLn "No URL specified"
-    url:_ -> do
-      putStrLn $ "Navigating to: " ++ url
-      r <- get url
-      putStrLn (show (r ^. responseBody))
-      -- putStrLn (r ^. responseBody . key "url" . _String)
-
-      -- let parsedUrl = parseURI url
-      --     scheme = (fromMaybe "Invalid URL" $ (dropLastColon . uriScheme) <$> parsedUrl)
-      --     host = maybe "Invalid URL" uriRegName (uriAuthority =<< parseURI url)
-      --     path = (fromMaybe "Invalid URL" $
-      --             (('/' :) . DL.intercalate "/" . pathSegments) <$> parsedUrl)
-      -- -- putStrLn $ "URI Scheme: " ++ scheme
-      -- -- putStrLn $ "Host: " ++ host
-      -- -- putStrLn $ "Path: " ++ path
-
-      -- -- Create network connection to host.
-      -- (sock, addr) <- createSocket scheme host
-      -- connect sock (addrAddress addr)
-      -- let request = "GET " ++ path ++ " HTTP/1.1\r\nHost: " ++ host ++ "\r\n\r\n"
-      -- sendAll sock (BS.pack request)
-      -- response <- recvAllChunks sock 4096
-      -- close sock
-
-      -- -- Handle response content.
-      -- let servResp = word8ArrayToString $ BL.unpack $ mconcat response
-      -- let linesList = lines servResp
-      -- let [httpVer, statusCode, statusMsg, date, server, lastMod, acceptRanges] = take 7 linesList
-      -- let [httpVersion, status, explanation] = take 3 $ words httpVer
-      -- let content = unlines $ drop 8 linesList
-      -- mapM_ putStrLn [httpVersion, status, explanation, statusCode, statusMsg, date, server, lastMod, acceptRanges]
-      -- -- putStrLn content
-
-
--- testUrl1 = "http://www.example.com:8080/path/to/page?query=value#fragment"
--- testUrl2 = "http://www.example.com"                           -- small HTTP page
--- testUrl3 = "http://eap.mcgill.ca/MagRack/COG/COG_P_96_05.htm" -- large HTTP page
--- testUrl4 = "http://eap.mcgill.ca/MagRack/COG/cognitio.htm"    -- large HTTP page
--- testUrl5 = "http://reduction.io/essays/rosetta-haskell.html"  -- different chunk size
